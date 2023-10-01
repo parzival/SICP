9944ee64adb2cb11788f1d7acac3bceb8f8a3a88
Michael Winterstein <parzival@quirksand.net>
2023-10-01 12:32:27 -0700

    Add sections 3.5.4 and 3.5.5

---
+++ solutions/3-5-4_sol.scm
@@ -0,0 +1,386 @@
+; Section 3.5.4.
+
+(display "Including library files...")
+
+(define lib-path "library/") ; REDEFINE AS NEEDED
+
+; Load-from-lib for MIT Scheme and most others
+(define (load-from-lib filename)
+  (load (string-append lib-path filename))
+  )
+
+; Load-from-lib for Racket/Pretty Big or similar
+;(define (load-from-lib filename)
+;  (load (string->path (string-append lib-path filename)))
+;  )
+
+(load-from-lib "stream_353.scm")
+(display-line "files finished loading.")
+(newline)
+
+; From text, integral using delay
+
+; Note that delay/force will be defined based on 'streambasic' file.
+
+(define (integral delayed-integrand initial-value dt) 
+  (define int
+    (cons-stream initial-value 
+                 (let ((integrand (force delayed-integrand)))
+                   (add-streams (scale-stream integrand dt) int)
+                   )
+                 )
+    )
+  int
+  )
+
+; Differential equation solver 
+(define (solve f y0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (stream-map f y))
+  y
+  )
+
+; Initial verification
+(display-line "Verifying integral (original version)")
+(display "Integral of stream 'ones' (expected: whole numbers):")
+
+(display-n-of-stream 10 (integral (delay ones) 0 1))  ; Should be the whole numbers
+
+
+; Defining a common set of inputs
+
+; Creates a stream of linearly-spaced floating-point values (for an integral or similar numeric function)
+(define (point-stream dx)
+  (scale-stream (cons-stream 0 integers) dx)
+  )
+
+; Converts a floating-point value to a numerical index 
+(define (to-index x)
+  (inexact->exact (round x))
+  )
+
+; Produces a properly spaced set of index values to check for math operations
+; The input list is a set of numeric values. The output produced can be used on a stream as long as the step size matches.
+; This allows us to vary the step size without having to change which values we are checking.
+(define (generate-check-list step-size li)
+  (map (lambda (x) (to-index (/ x step-size))) li)
+  )
+
+
+(define integral-step 0.01) ; step size used for all subsequent integral operations 
+
+(define ps1 (point-stream integral-step))
+(define integral-check-points (generate-check-list integral-step (list 0 0.5 1.0 1.5 2.0 2.5 3.0)))
+; s1 : 3x^2 + 2x - 1
+(define (f1 x) (+ (* 3 x x) (* 2 x) -1))
+; s2 - expected result : x^3 + x^2 - x  (Constant set to 0 by initial value)
+(define (int-f1 x) (+ (expt x 3) (expt x 2) (- x)))
+
+(newline)
+(display-line "Integral of a polynomial")
+(display-line "s1 is the result of integral")
+(display-line "s2 is the expected value (calculated from the solution function)")
+(show-streams-at-points (integral (delay (stream-map f1 (point-stream integral-step))) 0 integral-step)
+                        (stream-map int-f1 (point-stream integral-step))
+                        integral-check-points
+                        )
+
+
+
+; Verification of solve function with delay
+(define timestep 0.001)
+
+(define euler-num 2.718281828459)
+
+(newline)
+(display-line "Verifying original solve, using calculation of e.")
+(display "Known value of e to 12 decimal places: ")
+(display euler-num)
+(newline)
+(display "Calculated value: ")
+(define calced-e (stream-ref (solve (lambda (y) y) 1.0 timestep) (to-index (/ 1.0 timestep)) ) )
+(display calced-e)
+(newline)
+(display "Difference is ")
+(display (abs (- euler-num calced-e)))
+(newline)
+
+(newline)
+(display-line "Verifying original solve, using another equation")
+(define (f2 y) (+ (* 3 (expt y 2)) (* 2 y) 1))   ; f2: 3y^2 + 2y + 1
+(define c0 (atan (* 2 (sqrt 2)))) ; Constant, giving initial value of 1.0
+(define (fsol t) (/ (- (* (sqrt 2) (tan (+ (* (sqrt 2) t ) c0))) 1) 3.0) )  ; This is only valid up to (pi/2 - c0)/sqrt 2 ~ .240
+
+(display-line "s1 is the result of using (solve)")
+(display-line "s2 is the expected value (calculated from the solution function)")
+(define diffeq-check-points (generate-check-list timestep (list 0.0 0.02 0.05 0.07 0.10 0.13 0.16 0.18 0.20 0.22 0.23)))
+(show-streams-at-points  (solve f2 1.0 timestep) 
+                         (stream-map  fsol (scale-stream (cons-stream 0 integers) timestep))
+                         diffeq-check-points
+                         )
+
+; Ex 3.77.
+; Alternate method for integral
+
+; Non-delayed:
+;(define (integral integrand initial-value dt)
+;  (cons-stream initial-value
+;               (if (stream-null? integrand) 
+;                   the-empty-stream 
+;                   (integral (stream-cdr integrand)
+;                             (+ (* dt (stream-car integrand)) initial-value)
+;                             dt
+;                             )
+;                   )
+;               )
+;  )
+
+(define (integral delayed-integrand initial-value dt)
+  (cons-stream initial-value
+               (let ((integrand (force delayed-integrand)))
+                 (if (stream-null? integrand) 
+                     the-empty-stream 
+                     (integral
+                      (delay (stream-cdr integrand))
+                      (+ (* dt (stream-car integrand))
+                         initial-value)
+                      dt
+                      )
+                     )
+                 )
+               )
+  )
+
+
+; Verifying
+(newline)
+(display-line "Checking integral method")
+(display-line "Integral of polynomial")
+(display-line "s1 is the result of integral")
+(display-line "s2 is the expected value (calculated from the solution function)")
+(show-streams-at-points (integral (delay (stream-map f1 (point-stream integral-step))) 0 integral-step)
+                        (stream-map int-f1 (point-stream integral-step))
+                        integral-check-points
+                        )
+
+(newline)
+(display-line "Checking solve using new integral method (calculating e)")
+(display "Known value of e to 12 decimal places: ")
+(display euler-num)
+(newline)
+(display "Calculated value: ")
+(define calced-e-revised (stream-ref (solve (lambda (y) y) 1.0 timestep) (to-index (/ 1.0 timestep)) ) )
+(display calced-e)
+(newline)
+(display "Difference is ")
+(display (abs (- euler-num calced-e-revised)))
+(newline)
+
+
+(newline)
+(display-line "Checking solve (with new integral method) using another equation")
+(display-line "s1 is the result of using (solve)")
+(display-line "s2 is the expected value (calculated from the solution function)")
+(show-streams-at-points (solve f2 1.0 timestep) 
+                        (stream-map  fsol (scale-stream (cons-stream 0 integers) timestep))
+                        diffeq-check-points
+                        )
+
+; Ex 3.78.
+; Differential solver
+
+(define (solve-2nd a b y0 d-y0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (integral (delay ddy) d-y0 dt))
+  (define ddy (add-streams (scale-stream dy a)
+                           (scale-stream y b)
+                           )
+    )
+  y
+  )
+
+; Testing solver
+(newline)
+(display-line "Testing 2nd-order equation solver")
+(set! timestep 0.01)
+
+(define test-2nd-order (solve-2nd 4 -4 1 2 timestep))  ; y'' - 4y' + 4y = 0 -> y = (e^(2t))
+
+(define fsolution-t2o (stream-map  (lambda (t) (expt euler-num (* 2 t))) (scale-stream (cons-stream 0 integers) timestep)))
+
+(define test-2nd-order-points (generate-check-list timestep (list 0 2 5 10 20 30 50 80 100 120 167 213 280)))
+
+(display-line "s1 is the result of the solver")
+(display-line "s2 is the expected result (from solution function)")
+
+(show-streams-at-points test-2nd-order
+                        fsolution-t2o
+                        test-2nd-order-points
+                        )
+
+; Calculate cumulative error
+(display "Amount of relative error at t=3.0 : ")
+(display (abs (- 1 (/ (stream-ref test-2nd-order (to-index (/ 3.0 timestep))) (* (expt euler-num 6))))))
+(newline)
+
+; Ex 3.79.
+; General 2nd-order solver
+
+(define (gen-solve-2nd f y0 d-y0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (integral (delay ddy) d-y0 dt))
+  (define ddy (stream-map f dy y))
+  y
+  )
+
+; Testing general 2nd-order solver
+(newline)
+(display-line "Solving 2nd-order differential equations (generic approach)")
+
+; Same function as before (in 3.78)
+(define (lin2nd dy y)
+  (+ (* dy 4) (* y -4))
+  )
+
+(define test-generic-2nd-order (gen-solve-2nd lin2nd 1 2 timestep))
+
+(display-line "s1 is the result of the solver")
+(display-line "s2 is expected (from solution function)")
+(show-streams-at-points test-generic-2nd-order
+                           fsolution-t2o
+                           test-2nd-order-points
+                           )
+
+(display "Amount of relative error at t=3.0 : ")
+(display (abs (- 1 (/ (stream-ref test-generic-2nd-order (to-index (/ 3.0 timestep))) (* (expt euler-num 6))))) )
+(newline)
+
+; Non-linear function: Duffing equation with damping, no forcing
+; (models an oscillator with two stable positions, a 'double well')
+; This does not have an easily-expressed exact solution, but the damping
+; will lead to a final state in one of the two 'wells', given enough time.
+
+; Generates a damped equation function, with gamma as the damping coefficient
+(define (make-damped-duffing gamma)
+  (lambda (dy y)
+    (+ (- (* gamma dy))
+     y
+     (- (expt y 3))
+     )
+    )
+  )
+
+(newline)
+(display-line "Examining solution of non-linear equations")
+
+(define initial-vel 4.0)
+
+(define osc1 (gen-solve-2nd (make-damped-duffing 0.06) 1 initial-vel timestep))
+(define osc2 (gen-solve-2nd (make-damped-duffing 0.20) 1 initial-vel timestep))
+  
+(define oscillator-test-points (generate-check-list timestep '(0 1 2 3 4 5 10 20 50 100 101 102 103 104 105 110 500)))
+
+(display-line "s1 and s2 have different conditions, but approach the same (absolute) values")
+(show-streams-at-points osc1 osc2 oscillator-test-points)
+
+  
+; Ex 3.80.
+; RLC circuit solver
+
+(define (RLC r l c dt)
+  (lambda (vc0 i0)
+    (define vc (integral (delay dvc) vc0 dt))
+    (define il (integral (delay dil) i0 dt))
+    (define dvc (scale-stream il (/ -1 c)))
+    (define dil (add-streams (scale-stream vc (/ 1.0 l))
+                             (scale-stream il (/ (- r) l))
+                             )
+      )
+      (cons vc il)
+    )
+  )
+
+; Testing
+(newline)
+(display-line "Testing RLC")
+; R = 1 ohm, C = 0.2 farad, L = 1 henry, dt = 0.1 second
+(define test-circuit (RLC 1.0 1.0 0.2 0.1))
+
+(display-line "All circuits have Il0 = 0A, Vc0 = 10V.")
+(display-line "Initial example")
+; Initial values iL0 = 0, vC0 = 10V 
+(define results (test-circuit 10 0))
+(display "Voltage: ")
+(display-n-of-stream 40 (stream-reduce (car results) 10))
+(display "Current: ")
+(display-n-of-stream 40 (stream-reduce (cdr results) 10))
+
+(newline)
+(display-line "Checking variation in circuit values")
+
+(define test-circuit-ud  (RLC 1.0 0.5 0.2 0.1))  ; underdamped
+
+(define test-circuit-cd (RLC 2.0 0.5 0.5 0.1)) ; critically damped
+
+; overdamped
+(define test-circuit-expl (RLC 10.0 0.2 1.0 0.1))  ; what should happen vs what does 
+(define test-circuit-fix (RLC 10.0 0.2 1.0 0.01)) ; (how to fix it)
+
+(define results-under (test-circuit-ud 10 0))
+(define results-crit (test-circuit-cd 10 0))
+(define results-over (test-circuit-expl 10 0))
+(define results-fix-over (test-circuit-fix 10 0))
+
+(display-line "An underdamped system (similar to original example)")
+(display "Voltage: ")
+(display-n-of-stream 20 (stream-reduce (car results-under) 10))
+(display "Current: ")
+(display-n-of-stream 20 (stream-reduce (cdr results-under) 10))
+
+(display-line "A critically damped system")
+(display "Voltage: ")
+(display-n-of-stream 20 (stream-reduce (car results-crit) 10))
+(display "Current: ")
+(display-n-of-stream 20 (stream-reduce (cdr results-crit) 10))
+
+
+(display-line "A heavily overdamped system (with problems)")
+(display "Voltage: ")         
+(display-n-of-stream 20 (stream-reduce (car results-over) 10))
+(display "Current: ")
+(display-n-of-stream 20 (stream-reduce (car results-over) 10))
+
+(display-line "A heavily overdamped system (problem fixed)")
+(display "Voltage: ")
+(display-n-of-stream 20 (stream-reduce (car results-fix-over) 100))
+(display "Current: ")
+(display-n-of-stream 20 (stream-reduce (cdr results-fix-over) 100))
+
+; Optional for Racket (generates plots of RLC results)
+;(display-line "Plotted values")
+;(require plot)
+;(define rlc-x (get-n-of-stream 100 (point-stream 0.1)) )
+;(define cd-v1 (get-n-of-stream 100 (car results-crit)) )  
+;(define cd-i1 (get-n-of-stream 100 (cdr results-crit)) )
+
+;(display-line "Critically damped circuit")
+;(plot (points (map vector rlc-x cd-v1 )) )
+;(plot (points (map vector rlc-x cd-i1 )) )
+
+;(define ud-v1 (get-n-of-stream 100 (car results-under)))
+;(define ud-i1 (get-n-of-stream 100 (cdr results-under)))
+
+;(display-line "Underdamped circuit")
+;(plot (points (map vector rlc-x ud-v1)) )
+;(plot (points (map vector rlc-x ud-i1)) )
+
+;;The higher reduce value here is not due to the timestep difference, but because the
+;; overdamped system takes far longer to change.
+;(define rlc-x-dt (get-n-of-stream 100 (stream-reduce (point-stream 0.01) 100)) )
+;(define od-v1 (get-n-of-stream 100 (stream-reduce (car results-fix-over) 100)) )
+;(define od-i1 (get-n-of-stream 100 (stream-reduce (cdr results-fix-over) 100)) )
+
+;(display-line "Heavily overdamped circuit (note time scale)")
+;(plot (points (map vector rlc-x-dt od-v1)) )
+;(plot (points (map vector rlc-x-dt od-i1)) )
+
---
+++ solutions/3-5-5_sol.scm
@@ -0,0 +1,369 @@
+; Section 3.5.5.
+
+(display "Including files...")
+
+(define lib-path "library/") ; REDEFINE AS NEEDED
+
+; Load-from-lib for MIT Scheme and most others
+(define (load-from-lib filename)
+  (load (string-append lib-path filename))
+  )
+
+; Load-from-lib for Racket/Pretty Big or similar
+;(define (load-from-lib filename)
+;  (load (string->path (string-append lib-path filename)))
+;  )
+
+(load-from-lib "stream_353.scm")
+
+(display-line "...file loading finished.")
+(newline)
+
+; Creates a stream from a given index to the final index, inclusive
+(define (sub-stream start-index end-index str)
+  (if (> start-index 0)
+      (sub-stream (- start-index 1) (- end-index 1) (stream-cdr str))
+      (truncate-stream (+ end-index 1) str)
+    )
+  )
+
+; For random numbers, Racket/Pretty Big only needs this function.
+; (random) and (make-pseudo-random-generator) are defined.
+;(define (set-random-seed seed prg)
+;  (let ((old-prg (current-pseudo-random-generator))
+;        )
+;    (current-pseudo-random-generator prg)
+;    (random-seed seed)
+;    (current-pseudo-random-generator old-prg)
+;    )
+;  )
+
+; Define these for other interpreters, or modify as required
+
+(define (make-pseudo-random-generator)
+  (make-mwc-generator (remainder (current-jiffy) (expt 2 24)) (expt 2 23))
+  )
+ 
+(define (random prg)
+  (prg 'generate)
+  )
+  
+(define (set-random-seed seed prg)
+  ((prg 'set-seed) seed)
+  )
+
+; A PRNG, usable if the Scheme interpreter does not have a built-in PRNG
+(define (make-mwc-generator x0 c0)
+  (let ((b (expt 2 24))
+        (x x0)
+        (c c0)
+        (a 123)
+        )
+        
+    (define (generate-unscaled) ; Gives the raw value (24-bit integer)
+      (let ((xm (+ (* a x) c))
+            )
+        (set! x (remainder xm b))
+        (set! c (quotient xm b))
+        )
+        x
+      )
+      
+    (define (generate) ; Produces decimal values in [0,1)
+        (exact->inexact (/ (generate-unscaled) b))
+      )
+      
+    (define (set-seed nx)
+      (if (>= 1.0 nx) ; Seed can be either integer or decimal in range [0,1)
+        (set-seed-raw nx)
+        (set-seed-raw (* b nx))
+      )
+    )
+    
+    (define (set-seed-raw nx)
+      (if (< nx 0)
+         (error "Random seed for mwc-generator may not be negative. Seed given was : " nx)
+         (begin
+           (set! x (remainder nx b))
+           (set! c (if (= nx 0) c0 (quotient nx b)))
+           )
+         )
+      )
+      
+    (define (reset)
+      (set! x x0)
+      (set! c c0)
+      )
+      
+    (define (dispatch arg)
+      (cond
+        ((eq? arg 'generate)  (generate))
+        ((eq? arg 'generate-raw) (generate-unscaled))
+        ((eq? arg 'reset) (reset))
+        ((eq? arg 'set-seed) set-seed)
+        (else
+         (error "Unknown argument for mwc-generator :" arg)
+         )
+        )
+      )
+    dispatch
+    )
+  )
+
+; Define as needed
+;(define square sqr)
+
+(define pi 3.141592)
+
+
+; Ex 3.81
+; Repeatable random numbers from a stream
+
+; From Ex 3.6, for reference :
+
+;(define (rand arg)
+;  
+;  (define (generate) (random))
+;  
+;  (define (reset new-value) 
+;    (random-seed new-value)  
+;    )
+;    
+;  (cond
+;    ((eq? arg 'generate)  (generate))
+;    ((eq? arg 'reset) reset)
+;    (else
+;     (error "Unknown argument for rand :" arg)
+;     )
+;    )
+;  )
+
+(define (rand-gen request-stream)
+  
+  (let ((prg (make-pseudo-random-generator))
+        )
+    
+    (define (rand request)
+      (define (generate)
+        (random prg)
+        ) 
+      
+      (define (reset new-val)
+        (set-random-seed new-val prg)
+          new-val
+          ; 'reset
+        )
+
+      (cond
+        ((eq? request 'generate)  (generate))
+        ((and (list? request) (eq? (car request) 'reset)) (reset (cadr request)))
+        ((and (pair? request) (eq? (car request) 'reset)) (reset (cdr request)))
+        (else
+         (error "Unknown argument for rand :" request)
+         )
+        )
+      )
+    
+    
+    (stream-map rand request-stream)
+    ;(stream-filter number? (stream-map rand request-stream))
+    
+    )
+  )
+
+
+
+; Testing 
+
+; Produces an infinite stream of generate requests
+(define constant-gen
+  (cons-stream 'generate constant-gen)
+  )
+
+; Stream with given starting seed
+(define (con-gen-with-seed r)
+  (cons-stream (list 'reset r) constant-gen)
+  )
+
+; Used to convert abbreviated forms
+(define (expand-abbrev-request req)
+  (cond ((list? req) (map expand-abbrev-request req))
+        ((eq? req 'r) 'reset)
+        ((eq? req 'g) 'generate)
+        (else req)
+        )
+  )
+
+(define (make-request-stream li)
+  (list-to-stream (map expand-abbrev-request li))
+  )
+
+; These are using abbreviated labels, but will be expanded to full-word requests
+(define rng-test-requests (make-request-stream '((r 3) g g g (r 3) g g g (r 49) g g (r 593785) g (r 49) g g (r 54) g g)))
+
+(define rg-stream (rand-gen rng-test-requests))
+
+(display-line "Verifying reset and generate sequence")
+(display-stream rg-stream)
+
+(display-line "Testing generator produces same output after resets:")
+
+; NOTE: Use this version if reset requests do NOT produce output.
+;(display "Testing if reset and repeat works: ")
+;(display (stream-test = (sub-stream 0 2 rg-stream) (sub-stream 3 5 rg-stream)))
+;(newline)
+;(display "Testing if second reset (with a different seed in-between) works: ")
+;(display (stream-test = (sub-stream 6 7 rg-stream) (sub-stream 9 10 rg-stream)))
+;(newline)
+; END of tests relying on reset not producing output
+
+; NOTE: Use this version if reset requests do produce output.
+(display "Testing if reset and repeat works: ")
+(display (stream-test = (sub-stream 0 3 rg-stream) (sub-stream 4 7 rg-stream)))
+(newline)
+(display "Testing if second reset (with a different seed in-between) works: ")
+(display (stream-test = (sub-stream 8 10 rg-stream) (sub-stream 13 15 rg-stream)))
+(newline)
+; END of tests relying on reset producing output
+
+(newline)
+; Is stream ready to process from start?
+(display-line "Checking stream can take any initial request")
+
+; Create two streams using constant-gen as input stream 
+(define rs-congen-1 (rand-gen constant-gen))
+(define rs-congen-2 (rand-gen constant-gen))
+
+(display-line "Observing different streams starting with 'generate (match not required)")
+(display-n-of-stream 4 rs-congen-1)
+(display-n-of-stream 4 rs-congen-2)
+
+; Create two streams using con-gen-with-seed as input stream (use the same seed for #)
+(define rs-congen-s-1 (rand-gen (con-gen-with-seed 7)) )
+(define rs-congen-s-2 (rand-gen (con-gen-with-seed 7)) )
+
+(display-line "Observing different streams starting with 'reset (match not required)")
+(display-n-of-stream 4 rs-congen-s-1)
+(display-n-of-stream 4 rs-congen-s-2)
+(newline)
+
+; Are stream values preserved properly? (memoization may play a role)
+(define rand-out-stream (rand-gen (con-gen-with-seed 50000)))
+(display-line "Observing stream-ref behavior")
+(define y1 (stream-ref rand-out-stream 5))
+(display "At index 2: ")
+(display-line (stream-ref rand-out-stream 2))
+(display "Repeated at 2: ")
+(display-line (stream-ref rand-out-stream 2))
+(display "At 500: ")
+(display-line (stream-ref rand-out-stream 500))
+(display "At 100: ")
+(display-line (stream-ref rand-out-stream 100))
+(define y2 (stream-ref rand-out-stream 5))
+
+(display "Testing if stream-ref values remain consistent after access: ")
+(display (eq? y1 y2))
+(newline)
+
+; Do rng streams interfere with each other?
+(display-line "Observing two streams in concurrence")
+(define rng-requests (make-request-stream '((r 51) g g g g g (r 51) g g g g g) ))
+(define rng-requests-2 (make-request-stream '((r 12) g g (r 16) g g (r 12) g g) )) 
+
+(define nr-stream (rand-gen rng-requests))
+(define nr2-stream (rand-gen rng-requests-2))
+
+; NOTE: if 'reset' does NOT produce output, use this version
+;(define r1s1  (stream-ref nr-stream 3))  ; reset +4
+;(define r1s2  (stream-ref nr-stream 4))  ; reset +5
+;(define r2s1  (stream-ref nr2-stream 0)) ; reset +1
+;(stream-ref nr-stream 5) ; get to the reset command
+;(define r2s2 (stream-ref nr2-stream 4))  ; reset +1
+;(define r1s3  (stream-ref nr-stream 8)) ; reset +4
+;(stream-ref nr2-stream 4)
+;(stream-ref nr2-stream 0)
+;(define r1s4 (stream-ref nr-stream 9)) ; reset +5
+; END of test set-up relying on 'reset' not producing output
+
+; NOTE: if 'reset' does produce output, use THIS version
+(define r1s1  (stream-ref nr-stream 3))
+(define r1s2  (stream-ref nr-stream 4))
+(define r2s1  (stream-ref nr2-stream 1)) ; reset +1
+(stream-ref nr-stream 6) ; the reset command
+(define r2s2 (stream-ref nr2-stream 7))  ; reset +1
+(define r1s3  (stream-ref nr-stream 9)) ; reset +3
+(stream-ref nr2-stream 8)
+(stream-ref nr2-stream 1)
+(define r1s4 (stream-ref nr-stream 10)) ; reset +4
+; END of test set-up relying on 'reset' producing output
+
+(display-line "Testing if two streams interfere with each other")
+(display "Stream 1 ok:")
+(display (and (= r1s1 r1s3) (= r1s2 r1s4)))
+(newline)
+
+(display "Stream 2 ok:")
+(display (= r2s1 r2s2))
+(newline)
+
+; Ex 3.82
+; Monte Carlo integration with streams
+
+(define (monte-carlo experiment-stream passed failed)
+  (define (next passed failed)
+    (cons-stream (/ passed (+ passed failed)) 
+                 (monte-carlo
+                  (stream-cdr experiment-stream) passed failed
+                  )
+                 )
+    ) 
+  (if (stream-car experiment-stream)
+      (next (+ passed 1) failed)
+      (next passed (+ failed 1))
+      )
+  )
+
+; This will scale a number in [0,1] to the specified range
+(define (random-in-range low high rng-stream)
+   (let ((range (- high low)))
+    (+ low (* range (stream-car rng-stream)))
+    )
+  )
+
+
+(define (estimate-integral-stream P x1 x2 y1 y2)
+    (define (experiment-stream rand-stream)
+      (cons-stream 
+       (P (random-in-range x1 x2 rand-stream)
+          (random-in-range y1 y2 (stream-cdr rand-stream))
+          )
+       (experiment-stream (stream-cdr (stream-cdr rand-stream))) ; taking two values at a time
+       )
+      )
+  
+     (let ((total-area (* (- x2 x1) (- y2 y1)))
+        )
+       (scale-stream (monte-carlo (experiment-stream (rand-gen constant-gen)) 0 0) total-area)
+    )
+  )
+
+
+(define pi-estimate
+  (estimate-integral-stream
+                 (lambda (x y) (< (+ (square x) (square y)) 1.0))
+                            -2.0
+                            2.0
+                            -2.0
+                            2.0
+                            )
+  )
+
+; Testing
+(newline)
+(display-line "Using Monte Carlo integration to estimate pi (increasing number of iterations)")
+
+(for-each (lambda (x) (display-line (stream-ref pi-estimate x))) '(10 100 1000 10000 100000) )
+
+; Note: there is a small chance this will fail due to randomness
+(display "Checking if pi estimate is close (within 0.05):")
+(display (>= 0.05 (abs (- (stream-ref pi-estimate 100000)  pi))))
